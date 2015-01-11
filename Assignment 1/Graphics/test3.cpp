a3e8584da5edc1d55643171c092db6c48606ab3c
pratverma <pratverma@outlook.com>
2015-01-11 13:41:48 +0530

    Dudap

--- Assignment 1/Graphics/test3.cpp
+++ Assignment 1/Graphics/test3.cpp
@@ -13,7 +13,7 @@ void display(void)
     int const window_width  = glutGet(GLUT_WINDOW_WIDTH);
     int const window_height = glutGet(GLUT_WINDOW_HEIGHT);
     float const window_aspect = (float)window_width / (float)window_height;
-    cout<<window_width<<"   "<<window_height<<endl;
+    // cout<<window_width<<"   "<<window_height<<endl;
     glClearColor(0.5, 0.5, 1.0, 1.0);
     glClearDepth(1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -22,9 +22,9 @@ void display(void)
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
+    // gluOrtho2D(-600,600,-1200,1200);
     if(window_aspect > 1.) {
-        glOrtho(-window_aspect, window_aspect, -1, 1, -1, 1);
+        glOrtho(-1200, 1200, -600, 600, -1200, 1200);
     }
     else {
         glOrtho(-1, 1, -1/window_aspect, 1/window_aspect, -1, 1);
@@ -33,23 +33,50 @@ void display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    GLfloat const light_pos[4]     = {-1.00,  1.00,  1.00, 0.};
-    GLfloat const light_color[4]   = { 0.85,  0.90,  0.70, 1.};
+    GLfloat const light_pos[4]     = {1200, -600, 0    , 1.0  };
+    GLfloat const light_color[4]   = { 0,  0,  1, 1.};
     GLfloat const light_ambient[4] = { 0.10,  0.10,  0.30, 1.};
     glLightfv(GL_LIGHT0, GL_POSITION, light_pos),
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color);
     glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
 
+
+    GLfloat const light_pos2[4]     = {-1200, -600,  0  , 1.0  };
+    GLfloat const light_color2[4]   = { 1,  0,  0, 1.};
+    GLfloat const light_ambient2[4] = { 0.10,  0.10,  0.30, 1.};
+    glLightfv(GL_LIGHT1, GL_POSITION, light_pos2),
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light_color2);
+    glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient2);
+
+    GLfloat const light_pos3[4]     = {0, 600,  0  , 1.0  };
+    GLfloat const light_color3[4]   = { 0,  1,  0, 1.};
+    GLfloat const light_ambient3[4] = { 0.10,  0.10,  0.30, 1.};
+    glLightfv(GL_LIGHT2, GL_POSITION, light_pos3),
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, light_color3);
+    glLightfv(GL_LIGHT2, GL_AMBIENT, light_ambient3);
+
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHT2);
 
     glEnable(GL_DEPTH_TEST);
 
     glPushMatrix();
-        glTranslatef(x/(float)window_height, y/(float)window_height, 0);
-        x+=1;
-        y+=1;
-        glutSolidSphere(0.1, 31, 10);
+        glTranslatef(x, y, 0);
+         glColor3f(0.75, 0.5, 0); 
+
+        // x+=1;
+        // y+=1;
+        glutSolidSphere(100, 100, 100);
+        glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(300, 0, 0);
+        // x+=1;
+        // y+=1;
+        glColor3f(0.2,0.8,0.3);
+        glutSolidSphere(100, 31, 10);
         glPopMatrix();
     // for(  ) 
     // {
