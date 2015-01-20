522881150ce6831be5f12f179f46d5b60da42080
pratverma <pratverma@outlook.com>
2015-01-20 14:50:50 +0530

    yo

--- Assignment 1/3D Case/Threads.cpp
+++ Assignment 1/3D Case/Threads.cpp
@@ -389,13 +389,8 @@ void display(void)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glBegin(GL_QUADS);
-<<<<<<< HEAD
     glNormal3f(0.0, 1.0f, 0.0f);
     glColor3f(1,1,1); 
-=======
-    glNormal3f(0.0, 1.0f, 0.0f); 
-    glColor3f(1,1,1);
->>>>>>> c991a4e250f6b9fecd430511f06b6c139e077485
     glTexCoord2f(1.0f, 0.0f);  
     glVertex3f( f , -f, -f);
     glTexCoord2f(1.0f, 1.0f); 
@@ -632,11 +627,9 @@ void display(void)
     glDisable(GL_TEXTURE_2D);
     glEnd();
 
-<<<<<<< HEAD
+
     glEnable(GL_COLOR_MATERIAL);
-=======
- glEnable(GL_COLOR_MATERIAL);
->>>>>>> c991a4e250f6b9fecd430511f06b6c139e077485
+
     for( int i=0;i<FinalBoard.GetNumberBalls();i++ ) 
     {
         glPushMatrix();
