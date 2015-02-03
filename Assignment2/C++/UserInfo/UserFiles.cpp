9b1b74614bec4dd19aed0a8a3a5f98e2147ff2b8
kartikeyagup <kartikeyagupta@hotmail.com>
2015-02-03 12:47:23 +0530

    Basic functions for user files

--- Assignment2/C++/UserInfo/UserFiles.cpp
+++ Assignment2/C++/UserInfo/UserFiles.cpp
@@ -2,10 +2,10 @@
 
 UserFiles::UserFiles()
 {
-	PathVector = std::vector<pair<std::string,std::string>>;
+	PathVector = std::vector<std::pair<std::string,std::string>>;
 }
 
-std::vector<pair<std::string,std::string>> UserFiles::GetPathVector()
+std::vector<std::pair<std::string,std::string>> UserFiles::GetPathVector()
 {
 	return PathVector;
 }
@@ -17,5 +17,75 @@ int UserFiles::GetNumberOfFiles()
 
 std::string UserFiles::GetLocalNth(int n)
 {
-	if (n<)
+	if (n<PathVector.size())
+	{
+		return PathVector[n].first;
+	}
+	else
+	{
+		return "";
+	}
+}
+
+std::string UserFiles::GetGlobalNth(int n)
+{
+	if (n<PathVector.size())
+	{
+		return PathVector[n].second;
+	}
+	else
+	{
+		return "":
+	}
+}
+
+std::pair<std::string, std::string> UserFiles::GetNth(int n)
+{
+	if (n<PathVector.size())
+	{
+		return PathVector[n];
+	}
+	else
+	{
+		return std::pair<std::string,std::string> ("","");
+	}
+}
+
+void UserFiles::UpdateNth(int n, std::string localnew, std::string globalnew)
+{
+	if (n<PathVector.size())
+	{
+		PathVector[n]=std::pair<std::string,std::string> (localnew,globalnew);
+	}
+}
+
+void UserFiles::UpdateNthLocal(int n, std::string localnew)
+{
+	if (n<PathVector.size())
+	{
+		PathVector[n].first=localnew;
+	}
+}
+
+void UserFiles::UpdateNthGlobal(int n, std::string globalnew)
+{
+	if (n<PathVector.size())
+	{
+		PathVector[n].second=globalnew;
+	}
+}
+
+void UserFiles::AddNew(std::string localnew,std::string globalnew)
+{
+	PathVector.push_back(std::pair<std::string, std::string> (localnew,globalnew));
+}
+
+void UserFiles::AddNewLocal(std::string localnew)
+{
+	PathVector.push_back(std::pair<std::string, std::string> (localnew,""));
+}
+
+void UserFiles::AddNewGlobal(std::string globalnew)
+{
+	PathVector.push_back(std::pair<std::string, std::string> ("",globalnew));
 }
--- Assignment2/C++/UserInfo/UserFiles.h
+++ Assignment2/C++/UserInfo/UserFiles.h
@@ -9,24 +9,25 @@ class UserFiles
 	public:
 		UserFiles();
 	
-		std::vector<pair<std::string,std::string>> GetPathVector();
+		std::vector<std::pair<std::string,std::string>> GetPathVector();
 		// std::vector<std::string> GetGlobalPath();
 
 		int GetNumberOfFiles();
 		std::string GetLocalNth(int);
 		std::string GetGlobalNth(int);
-		pair<std::string,std::string> GetNth(int);
+		std::pair<std::string,std::string> GetNth(int);
 
 		void UpdateNth(int,std::string, std::string);
 		void UpdateNthLocal(int, std::string);
 		void UpdateNthGlobal(int, std::string);
 
 		void AddNew(std::string,std::string);
+		void AddNewLocal(std::string);
+		void AddNewGlobal(std::string);
 
 	private:
-		std::vector<pair<std::string,std::string>> PathVector;
+		std::vector<std::pair<std::string,std::string>> PathVector;
 		//std::vector<std::string> GlobalPath;
 };
-
 
 #endif
