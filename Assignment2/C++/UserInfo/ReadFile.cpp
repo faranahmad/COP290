206d52f86632fd8f627c369cafd50ea18a717a05
kartikeyagup <kartikeyagupta@hotmail.com>
2015-02-05 12:44:51 +0530

    file discovery :P Prateek

---
+++ Assignment2/C++/UserInfo/ReadFile.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+static std::vector<char> ReadAllBytes(char const* filename)
+{
+    ifstream ifs(filename, ios::binary|ios::ate);
+    ifstream::pos_type pos = ifs.tellg();
+
+    std::vector<char>  result(pos);
+
+    ifs.seekg(0, ios::beg);
+    ifs.read(&result[0], pos);
+
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+	std::vector<char> ans = ReadAllBytes(argv[1]);
+	std::string data="";
+	for (int i=0; i<ans.size() ; i++ )
+	{
+		data+=ans[i];
+	}
+	std::ofstream out("Anu2.mp4");
+	out << data;
+	out.close();
+	return 0;
+}
