61f1f563f732c6ff14b9620c88f2553d90b142e6
pratverma <pratverma@outlook.com>
2015-03-01 13:05:32 +0530

    yo

--- Assignment2/C++/graphics/ServerFinal/onlineusers.cpp
+++ Assignment2/C++/graphics/ServerFinal/onlineusers.cpp
@@ -2,6 +2,8 @@
 #include "ui_onlineusers.h"
 
 //to display the list of online users
+extern std::vector<std::string> usersLog;
+
 OnlineUsers::OnlineUsers(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::OnlineUsers)
@@ -14,6 +16,10 @@ OnlineUsers::OnlineUsers(QWidget *parent) :
     ui->pushButton->setStyleSheet("background-color:light green;");//pushbutton is back button
     ui->listWidget->setStyleSheet("background-color:white;");
     ui->pushButton->setFocusPolicy(Qt::NoFocus);                                                                                                                                                             
+    for (unsigned int i=0; i< usersLog.size(); i++)
+    {
+        ui->listWidget->addItem(usersLog[i].c_str());
+    }
 }
 
 OnlineUsers::~OnlineUsers()
