0b4632c16b0ee01951775ecc54172cfd0ba2068b
faranahmad <faran.ahmad95@hotmail.com>
2015-03-01 18:02:40 +0530

    final server ui

--- Assignment2/C++/ServerFinal/onlineusers.cpp
+++ Assignment2/C++/ServerFinal/onlineusers.cpp
@@ -1,7 +1,7 @@
 #include "onlineusers.h"
 #include "ui_onlineusers.h"
 
-//to display the list of online users
+
 extern std::vector<std::string> usersLog;
 
 OnlineUsers::OnlineUsers(QWidget *parent) :
@@ -9,13 +9,15 @@ OnlineUsers::OnlineUsers(QWidget *parent) :
     ui(new Ui::OnlineUsers)
 {
     ui->setupUi(this);
-    this->setFixedSize(617,771);
-    this->move(670,0);
     this->setWindowIcon(QIcon("/home/faran/Desktop/COP290/Assignment2/DesignDocument/ddlogo2.png")) ;
+    this->setFixedSize(531,665);
+    this->move(670,0);
+    this->setObjectName("online");
+    this->setStyleSheet("#online {background-color:lightgreen;}");
     this->setStyleSheet("background-color:rgb(0,255,255);");
-    ui->pushButton->setStyleSheet("background-color:light green;");//pushbutton is back button
+    ui->pushButton->setStyleSheet("background-color:light green;");
     ui->listWidget->setStyleSheet("background-color:white;");
-    ui->pushButton->setFocusPolicy(Qt::NoFocus);                                                                                                                                                             
+    ui->pushButton->setFocusPolicy(Qt::NoFocus);
     for (unsigned int i=0; i< usersLog.size(); i++)
     {
         ui->listWidget->addItem(usersLog[i].c_str());
@@ -29,6 +31,5 @@ OnlineUsers::~OnlineUsers()
 
 void OnlineUsers::on_pushButton_clicked()
 {
-    //clicking on back button hides this window to go back to main window
     this->hide();
 }
--- Assignment2/C++/ServerFinal/server.cpp
+++ Assignment2/C++/ServerFinal/server.cpp
@@ -3,46 +3,42 @@
 #include "ui_server.h"
 #include "onlineusers.h"
 #include "serverfilesandfolders.h"
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <stdlib.h>
 #include <unistd.h>
-
 
 extern std::string iptodisplay; 
 extern std::string porttodisplay;
 extern std::vector<std::string> usersLog;
 extern char input;
 
+
+
 server::server(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::server)
 {
-    //constructor to set up the server side main window
     ui->setupUi(this);
     this->move(0,0);
     this->setFixedSize(608,661);
+    this->setWindowIcon(QIcon("/home/faran/Desktop/COP290/Assignment2/DesignDocument/ddlogo2.png")) ;
     this->setStyleSheet("background-color:rgb(0,255,255);");
     ui->allusers->setStyleSheet("background-color:light green;");
     ui->shutdown->setStyleSheet("background-color:light green;");
     ui->filesandfolders->setStyleSheet("background-color:light green;");
     ui->onlineusers->setStyleSheet("background-color:light green;");
-    ui->label_2->setText(iptodisplay.c_str());    //displays the ip address
-    ui->label_4->setText(porttodisplay.c_str());  // displays the port with which it is connected to client
-    ui->label_5->setText("Dead Drop");
+    ui->label_2->setText(iptodisplay.c_str());
+    ui->label_4->setText(porttodisplay.c_str());
+
 }
 
 server::~server()
 {
+
     input='q';
     usleep(1000);
-    delete ui;
-}
+    delete ui;}
 
 void server::on_shutdown_clicked()
 {
-    //to shut down the window
     input='q';
     usleep(1000);
     qApp->quit();
@@ -51,16 +47,14 @@ void server::on_shutdown_clicked()
 
 void server::on_allusers_clicked()
 {
-    //displays the list of all users 
-    allusers allusers1;            //opens a new window that displays the list of all users     
-    allusers1.setModal(true);      
+    allusers allusers1;
+    allusers1.setModal(true);
     allusers1.exec();
 }
 
 void server::on_filesandfolders_clicked()
 {
-    //displays the list of all files and folders stored on the server
-    serverfilesandfolders serverfilesandfolders1; //opens a new window that displays the list all files and folders storesd on the server
+    serverfilesandfolders serverfilesandfolders1;
     serverfilesandfolders1.setModal(true);
     serverfilesandfolders1.exec();
 
@@ -68,8 +62,7 @@ void server::on_filesandfolders_clicked()
 
 void server::on_onlineusers_clicked()
 {
-    //displays the list of all online users 
-    OnlineUsers onlineusers1;                     //opens a new window that displays the list of all users that are online
-    onlineusers1.setModal(true);                 
+    OnlineUsers onlineusers1;
+    onlineusers1.setModal(true);
     onlineusers1.exec();
 }
